9734d88573287f59b79537b1dbfe2171f55f1659
UnknownMonk <smodcartoon@gmail.com>
2019-01-13 16:06:46 -0500

    refactor react app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Todos from './Todos';
 
 class App extends Component {
+  state = {
+    todos: [
+      { id: 1, content: 'buy some milk' },
+      { id: 2, content: 'play mario kart' }
+    ]
+  };
+  deleteTodo = id => {
+    const todos = this.state.todos.filter(todo => {
+      return todo.id !== id;
+    });
+    this.setState({
+      todos
+    });
+  };
   render() {
+    const { todos } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo-app container">
+        <h1 className="center blue-text">Todo's</h1>
+        <Todos todos={todos} deleteTodo={this.deleteTodo} />
       </div>
     );
   }
 }
-
 export default App;
